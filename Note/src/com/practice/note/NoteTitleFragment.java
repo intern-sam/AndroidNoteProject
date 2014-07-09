92c4db214bd2eed522e017e78920f4408c56bc74
intern-sam <intern@10.0.1.6>
2014-07-09 13:39:59 -0700

    floating context menu to edit or delete note available on long click of a note.

--- Note/src/com/practice/note/NoteTitleFragment.java
+++ Note/src/com/practice/note/NoteTitleFragment.java
@@ -9,7 +9,13 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -24,6 +30,10 @@ public class NoteTitleFragment extends ListFragment {
 	private int layout;
 	private List<Note> values;
 	private boolean isPhone;
+	View someView = getView();
+	private Note note;
+
+	// private ActionMode mActionMode;
 
 	public interface OnListItemSelectedListener {
 		public void onListItemSelected(int position);
@@ -48,6 +58,7 @@ public class NoteTitleFragment extends ListFragment {
 			noteContentFragment.updateItemContentView(0);
 		}
 
+		registerForContextMenu(getListView());
 	}
 
 	@Override
@@ -76,7 +87,7 @@ public class NoteTitleFragment extends ListFragment {
 
 		mCursor = dataSource.allNotes();
 		mCursor.moveToPosition(position);
-		Note note = dataSource.getNote(position);
+		note = dataSource.getNote(position);
 
 		if (getActivity().findViewById(R.id.note_frag) == null) {
 			Intent intent = new Intent(getActivity(), NewNoteActivity.class);
@@ -100,6 +111,56 @@ public class NoteTitleFragment extends ListFragment {
 
 	}
 
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		MenuInflater inflater = getActivity().getMenuInflater();
+		AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+		note = dataSource.getNote(info.position);
+		menu.setHeaderTitle(note.getTitle());
+		inflater.inflate(R.menu.item_long_press_menu, menu);
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
+				.getMenuInfo();
+		switch (item.getItemId()) {
+		case R.id.edit:
+			mCursor = dataSource.allNotes();
+			mCursor.moveToPosition(info.position);
+			note = dataSource.getNote(info.position);
+
+			if (getActivity().findViewById(R.id.note_frag) == null) {
+				Intent intent = new Intent(getActivity(), NewNoteActivity.class);
+				intent.putExtra("POS", info.position);
+				intent.putExtra(MySQLiteHelper.COLUMN_ID, note.getId());
+				intent.putExtra(
+						MySQLiteHelper.COLUMN_TITLE,
+						mCursor.getString(mCursor
+								.getColumnIndexOrThrow(MySQLiteHelper.COLUMN_TITLE)));
+				intent.putExtra(
+						MySQLiteHelper.COLUMN_CONTENT,
+						mCursor.getString(mCursor
+								.getColumnIndexOrThrow(MySQLiteHelper.COLUMN_CONTENT)));
+				// note.updateItemContentView(position);
+				getActivity().startActivity(intent);
+				mCallback.onListItemSelected(info.position);
+				getListView().setItemChecked(info.position, true);
+			} else {
+				mCallback.onListItemSelected(info.position);
+			}
+			return true;
+		case R.id.delete:
+			dataSource.deleteNote(note);
+			updateNote();
+			return true;
+		default:
+			return super.onContextItemSelected(item);
+		}
+	}
+
 	public void createNote(Intent intent) {
 		@SuppressWarnings("unchecked")
 		ArrayAdapter<Note> adapter = (ArrayAdapter<Note>) getListAdapter();
@@ -114,7 +175,7 @@ public class NoteTitleFragment extends ListFragment {
 	}
 
 	public void updateNote() {
-		Note note = null;
+		note = null;
 		values = dataSource.getAllNotes();
 		ArrayAdapter<Note> adapter = new ArrayAdapter<Note>(getActivity(),
 				layout, values);
